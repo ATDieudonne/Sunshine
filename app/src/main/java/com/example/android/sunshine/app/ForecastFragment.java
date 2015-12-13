52d7da3e32e88c717b6b6f8d3c266532f8557659
Alexandre Dieudonne <adieudonne@gmail.com>
2015-12-13 18:55:38 -0500

    renamed MainActivityFragment to ForecastFragment

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -20,9 +20,9 @@ import java.util.ArrayList;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ForecastFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public ForecastFragment() {
     }
 
     @Override
