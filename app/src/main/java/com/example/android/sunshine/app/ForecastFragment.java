30631cd06797e836ae8289db95326c5434ea67ed
Alexandre Dieudonne <adieudonne@gmail.com>
2015-12-13 19:21:35 -0500

    Changed class param HttpURLConnection to Void, created LOG_TAG constant and use inplace of harcoded "PlaceholderFragment" tag.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -71,7 +71,9 @@ public class ForecastFragment extends Fragment {
     }
 
 
-    abstract class FetchWeatherTask extends AsyncTask<HttpURLConnection,Void,Void>{
+    abstract class FetchWeatherTask extends AsyncTask<Void,Void,Void>{
+
+        public final String LOG_TAG=FetchWeatherTask.class.getSimpleName();
         //Going to insert networking code snippet from Udacity's GitHub repo
 
         // These two need to be declared outside the try/catch
@@ -117,7 +119,7 @@ public class ForecastFragment extends Fragment {
                 }
                 forecastJsonStr = buffer.toString();
             } catch (IOException e) {
-                Log.e("PlaceholderFragment", "Error ", e);
+                Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
@@ -129,7 +131,7 @@ public class ForecastFragment extends Fragment {
                     try {
                         reader.close();
                     } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                        Log.e(LOG_TAG, "Error closing stream", e);
                     }
                 }
             }
